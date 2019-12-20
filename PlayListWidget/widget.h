85efe742c3a274786213735e64a21167c7693969
petarbdl <56541832+petarbdl@users.noreply.github.com>
2019-12-20 15:56:45 +0100

    Add files via upload

---
+++ PlayListWidget/widget.h
@@ -0,0 +1,63 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QFile>
+#include <QStandardItemModel>
+#include <QFileDialog>
+#include <QVector>
+#include <QLabel>
+#include <QLayout>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QMessageBox>
+#include <QStandardPaths>
+
+//Struct to keep Item name and duration
+struct Items
+{
+    QString fileName;//Name of the Video
+    QString fileDuration;//Duration of the Video
+    //Constructor for this Struck
+    Items(QString name, QString duration) {
+        fileName = name;
+        fileDuration = duration;
+    }
+};
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+    //This Function append new Item to the vector list
+    void appendFile(QString name, QString duration);
+
+private slots:
+    void on_pushButtonAdd_clicked();
+
+    void on_pushButtonClose_clicked();
+
+    void browseFiles();
+
+    void pushButtonFind_clicked();
+
+    void on_pushButtonUp_clicked();
+
+    void on_pushButtonDown_clicked();
+
+private:
+    Ui::Widget *ui;//The Widget
+    QStandardItemModel *_model;//Data Model
+    QVector<Items> _list;//List where Items are stored
+    QDialog *_dialog;//This Dialog is opened when Add Button is clicked
+    QLineEdit *lineEdit;//LineEdit used in the Dialog
+};
+#endif // WIDGET_H
